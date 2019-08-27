aeacb298ed69fb458cf1f56d4b035b3a1acd84c7
Muhammad Taha <muhammadtaha91@gmail.com>
2019-08-27 20:13:50 +0200

    collecting data from the gmap direction api

---
+++ gmapDirectionCall.py
@@ -0,0 +1,50 @@
+# import requests
+# url = 'http://ES_search_demo.com/'
+# data = '''{
+#   "query": {
+#     "bool": {
+#       "must": [
+#         {
+#           "text": {
+#             "record.document": "SOME_JOURNAL"
+#           }
+#         },
+#         {
+#           "text": {
+#             "record.articleTitle": "farmers"
+#           }
+#         }
+#       ],
+#       "must_not": [],
+#       "should": []
+#     }
+#   },
+#   "from": 0,
+#   "size": 50,
+#   "sort": [],
+#   "facets": {}
+# }'''
+# response = requests.post(url, data=data)
+
+
+import googlemaps
+from datetime import datetime
+import json
+
+
+gmaps = googlemaps.Client(key='AIzaSyCDpp8NnfzrHqGjLSSVwtQWrUeGPTsZntg')
+
+# now = datetime.now()
+now = datetime.strptime('Aug 27 2019 7:33PM', '%b %d %Y %I:%M%p')
+print(now)
+directions_result = gmaps.directions("Sydney Town Hall",
+                                     "Parramatta, NSW",
+                                     mode="driving",
+                                     departure_time=now)
+
+jsonResult = json.loads(json.dumps(directions_result[0]))
+
+print(jsonResult["legs"][0]["duration"]["text"])
+
+print(directions_result[0])
+# print(jsonResult)
